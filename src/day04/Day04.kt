a44813042fcee7211d2ab5bc079aba6ca9d36a80
Luke 🤘 <lucgirardin2@gmail.com>
2021-12-04 17:53:21 -0500

    Adds day 04 solution

---
+++ src/day04/Day04.kt
@@ -0,0 +1,105 @@
+package day04
+
+import arrow.core.identity
+import io.kotest.matchers.shouldBe
+import readInput
+
+typealias Winner = Pair<List<Int?>, Int>
+typealias Grid = List<Int?>
+
+fun main() {
+    fun part1(input: List<String>) = selectWinner(input, Sequence<Winner>::first)
+    fun part2(input: List<String>) = selectWinner(input, Sequence<Winner>::last)
+
+    val testInput = readInput("04", "test_input")
+    val input = readInput("04", "input")
+
+    part1(testInput) shouldBe 4512
+    println(part1(input))
+
+    part2(testInput) shouldBe 1924
+    println(part2(input))
+}
+
+fun selectWinner(input: List<String>, selector: Sequence<Winner>.() -> Winner): Int {
+    return parse(input)
+        .let(computeWinnerSequence)
+        .selector()
+        .let(encryptSolution)
+}
+
+fun parse(input: List<String>) = parseGrids(input) to parseDrawnNumbers(input)
+
+val computeWinnerSequence: (Pair<List<Grid>, Sequence<Int>>) -> Sequence<Winner> = { (grids, numberSequence) ->
+    sequence {
+        val gridsToMarks = grids.map {
+            it to Marks(MutableList(5) { 0 }, MutableList(5) { 0 })
+        }
+
+        numberSequence.fold(gridsToMarks) { accumulator, drawnNumber ->
+            val (winners, stillPlaying) = accumulator.map { entry ->
+                val grid = entry.first
+                grid.indexOf(drawnNumber)
+                    .takeIf { it in grid.indices }
+                    ?.let { index -> entry.mark(index) }
+                    ?: entry
+            }.partition { (_, marks) -> marks.hasWon }
+
+            winners.asSequence()
+                .map { it.first to drawnNumber }
+                .let { yieldAll(it) }
+
+            stillPlaying
+        }
+    }
+}
+
+val encryptSolution: (Winner) -> Int = { (winner, winningNumber) ->
+    winner.asSequence()
+        .filterNotNull()
+        .sum()
+        .times(winningNumber)
+}
+
+fun parseGrids(input: List<String>): List<Grid> {
+    return input.asSequence()
+        .drop(2)
+        .filter { it.isNotBlank() }
+        .map { line ->
+            numberRegex.findAll(line)
+                .mapTo(mutableListOf()) { it.value.toInt() }
+        }
+        .chunked(5) {
+            it.flatten()
+                .mapTo(mutableListOf<Int?>(), ::identity)
+        }
+        .toList()
+}
+
+fun parseDrawnNumbers(input: List<String>): Sequence<Int> {
+    return input.first()
+        .splitToSequence(",")
+        .map(String::toInt)
+}
+
+val numberRegex = "\\d+".toRegex()
+
+fun Pair<Grid, Marks>.mark(index: Int) = Pair(
+    first.toMutableList().apply { this[index] = null },
+    second.incrementedAt(index / 5, index % 5)
+)
+
+data class Marks(
+    val marksInRows: List<Int>,
+    val marksInColumns: List<Int>,
+) {
+    val hasWon: Boolean by lazy {
+        marksInRows.any { it >= marksInRows.size }
+            || marksInColumns.any { it >= marksInColumns.size }
+    }
+
+    fun incrementedAt(i: Int, j: Int) = copy(
+        marksInRows = marksInRows.mapIndexed { rowIndex, count -> if (rowIndex == i) count + 1 else count },
+        marksInColumns = marksInColumns.mapIndexed { columnIndex, count -> if (columnIndex == j) count + 1 else count },
+    )
+}
